e6665440614c13ed514ff629b0279dea79241b1b
xandre9-git <jmjames673@gmail.com>
2023-01-19 20:51:10 -0500

    Create left side bar, projects section, and add project button

--- src/pageLayout.js
+++ src/pageLayout.js
@@ -2,18 +2,38 @@
 
 const body = document.getElementById("content");
 
-const appBanner = document.createElement('div');
-appBanner.id = "top-banner"
-appBanner.style.height = "50px";
-appBanner.style.width = "100%";
-appBanner.style.backgroundColor = "gold";
+const topBar = document.createElement('div');
+topBar.id = "top-bar"
 
 const appTitle = document.createElement('h1');
 appTitle.textContent = "Mas To Do";
 appTitle.className = "title";
 
+const leftSideBar = document.createElement('div');
+leftSideBar.id = "left-side-bar";
 
-body.appendChild(appBanner);
-appBanner.appendChild(appTitle);
+const projectsTitle = document.createElement('div');
+projectsTitle.id = "projects-title"
+projectsTitle.textContent = "Projects"
+
+const projectsContainer = document.createElement('div');
+projectsContainer.id = "projects-container";
+
+const projectLists = document.createElement('ul');
+const addProjectButton = document.createElement('li');
+addProjectButton.id = "add-project";
+addProjectButton.textContent = "+ Add Project";
+projectLists.appendChild(addProjectButton);
+projectsContainer.appendChild(projectLists);
+
+
+
+body.appendChild(topBar);
+topBar.appendChild(appTitle);
+
+body.appendChild(leftSideBar);
+
+leftSideBar.appendChild(projectsTitle);
+leftSideBar.appendChild(projectsContainer);
 
 export { body };
