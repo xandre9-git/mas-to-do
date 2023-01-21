02f733fc45b6cdb28280005938d5871b880c559c
xandre9-git <jmjames673@gmail.com>
2023-01-21 12:18:31 -0500

    Set return of addProject function to string and array variables

--- src/todoModule.js
+++ src/todoModule.js
@@ -21,20 +21,49 @@ function addProject(){
   let projectName = prompt("Enter name of project:");
   // projectName is a string to use as the textContent of a created li
   if (projectName != null) {
-    console.log(projectName);
     projects.push(projectName);
   }
-
-  console.log(projects);
-  console.log(body);
-  
-  return projectName;
+  return { projectName, projects };
 }
 
 function createListItem(listName) {
   let listItem = document.createElement('li');
   listItem.textContent = listName;
+
+  // create project button container
+  let projectBtns = document.createElement('div');
+  projectBtns.className = "project-btns";
+
+  // create edit button
+  let projectEditBtn = document.createElement('button');
+  projectEditBtn.type = "button";
+  // projectEditBtn.className = "project-btns"
+  projectEditBtn.id = "edit-btn";
+  let editBtnImg = document.createElement('img');
+  editBtnImg.src = "/src/images/edit.svg";
+  editBtnImg.className = "project-btns filter-white";
+ 
+  editBtnImg.title = "Edit";
+  projectEditBtn.appendChild(editBtnImg);
+
+   // create delete button
+  let projectDelBtn = document.createElement('button');
+  projectDelBtn.type = "button";
+  // projectDelBtn.className = "project-btns";
+  projectDelBtn.id = "del-btn";
+  let delBtnImg = document.createElement('img');
+  delBtnImg.src = "/src/images/delete.svg";
+  delBtnImg.className = "project-btns filter-white";
+  delBtnImg.title = "Delete"
+  projectDelBtn.appendChild(delBtnImg);
+  
+  projectBtns.appendChild(projectEditBtn);
+  projectBtns.appendChild(projectDelBtn);
+  listItem.appendChild(projectBtns);
+
+
+ 
   return listItem;
 }
 
-export { addProject, createListItem };
+export { addProject, createListItem, projects };
