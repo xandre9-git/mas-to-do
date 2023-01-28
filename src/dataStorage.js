9f972dd0230e24ab3985fc00829a99382c5c6416
xandre9-git <jmjames673@gmail.com>
2023-01-28 14:42:32 -0500

    Create dataStorage.js to hold project data, will later be used for task data as well

---
+++ src/dataStorage.js
@@ -0,0 +1,20 @@
+import { addProject } from "./todoModule";
+
+let projects = [];
+
+// if ((window.localStorage.getItem("projectname")) != null) {
+//   projects = window.localStorage.getItem("projectname")
+//   console.log(`if statement section firing`);
+// }
+
+// console.log(`projects value in dataStorage: ${projects}`);
+
+// // Object Constructor
+// function Projects(name) {
+//   this.name = name;
+// }
+
+// // Example Project
+// const odinProject = new Projects('The Odin Project');
+
+export { projects }
