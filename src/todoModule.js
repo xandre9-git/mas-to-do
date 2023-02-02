03ca0185203bba602fee776a7655d04a76c60285
xandre9-git <jmjames673@gmail.com>
2023-02-01 19:09:31 -0500

    Format code, create function to add projects to DOM, and update exports

--- src/todoModule.js
+++ src/todoModule.js
@@ -1,9 +1,10 @@
 // TO DO MODULES
 
 import { projects } from "./dataStorage";
+import { projectsList } from "./pageLayout";
 
 // this function gets project name and stores as a string
-function addProject(){
+function addProject() {
   let projectName = prompt("Enter name of project:");
   // projectName is a string used as the textContent of created list item
   if (projectName != null) {
@@ -20,36 +21,64 @@ function projectListSetter(str, node) {
     node.prepend(project);
     return node;
   }
-} 
- 
+}
+
 // this function creates the list item on the DOM
 function createListItem(listName) {
-  let listItem = document.createElement('li');
+  let listItem = document.createElement("li");
   listItem.textContent = listName;
 
   // create project button container
-  let projectBtns = document.createElement('div');
+  let projectBtns = document.createElement("div");
   projectBtns.className = "project-btns-container";
 
   // create edit button
-  let projectEditBtn = document.createElement('div');
-  projectEditBtn.className = "project-btns, filter-white"
+  let projectEditBtn = document.createElement("div");
+  projectEditBtn.className = "project-btns, filter-white";
   projectEditBtn.id = "edit-btn";
   projectEditBtn.title = "Edit";
- 
+
   // create delete button
-  let projectDelBtn = document.createElement('div');
+  let projectDelBtn = document.createElement("div");
   projectDelBtn.className = "project-btns";
   projectDelBtn.id = "del-btn";
   projectDelBtn.className = "project-btns filter-white";
-  projectDelBtn.title = "Delete"
-  
+  projectDelBtn.title = "Delete";
+
   // append elements
   projectBtns.appendChild(projectEditBtn);
-  projectBtns.appendChild(projectDelBtn); 
+  projectBtns.appendChild(projectDelBtn);
   listItem.appendChild(projectBtns);
 
   return listItem;
+}
+
+// this function ties it all together
+function DOMProjectAdder() {
+  let res = addProject();
+  if (res != null) {
+    console.log(`res value: ${res}`);
+    window.localStorage.setItem("projectname", res);
+    // if statement to add item to array if no previous items exist
+    if (projects.length < 1) {
+      projects.push(res);
+      // use local storage
+      window.localStorage.setItem("projectnames", JSON.stringify(projects));
+      projectListSetter(res, projectsList);
+    }
+
+    // for loop and if statement to check if project does not already exist
+    for (let i = 0; i < projects.length; i++) {
+      console.log(projects.includes(res));
+      if (!projects.includes(res)) {
+        projects.push(res);
+        window.localStorage.setItem("projectnames", JSON.stringify(projects));
+        projectListSetter(res, projectsList);
+        document.location.reload();
+      }
+    }
+  }
+  return projectListSetter;
 }
 
 // delete project function
@@ -61,4 +90,4 @@ function deleteProject(projectname, arr) {
   return arr;
 }
 
-export { addProject, createListItem, projectListSetter, deleteProject };
+export { addProject, createListItem, projectListSetter, DOMProjectAdder, deleteProject };
