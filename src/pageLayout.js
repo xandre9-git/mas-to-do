fa48b7179e12b1da94b9300d58dc3d407d02006d
xandre9-git <jmjames673@gmail.com>
2023-02-04 11:15:43 -0500

    Create separate containers for current task and task details

--- src/pageLayout.js
+++ src/pageLayout.js
@@ -5,6 +5,7 @@ import { projectListSetter } from "./todoModule";
 import { DOMProjectAdder } from "./todoModule";
 import { deleteProject } from "./todoModule";
 
+
 console.log(JSON.parse(window.localStorage.getItem("projectnames")));
 if (JSON.parse(window.localStorage.getItem("projectnames")).length > 0) {
   window.projects = JSON.parse(window.localStorage.getItem("projectnames"));
@@ -41,6 +42,59 @@ const addProjectButton = document.createElement("li");
 addProjectButton.className = "add-projects";
 addProjectButton.textContent = "+ Add Project";
 
+const todoBoardsContainer = document.createElement('div');
+todoBoardsContainer.id = "to-dos-container";
+
+const currentContainer = document.createElement("div");
+currentContainer.className = "task-boxes-container";
+
+// current tasks DOM
+
+const currentTaskContainer = document.createElement("div");
+currentTaskContainer.id = "current-tasks"
+
+const currentTasksTitle = document.createElement("h2");
+currentTasksTitle.className = "to-do-title";
+currentTasksTitle.textContent = "Current Tasks";
+currentTaskContainer.appendChild(currentTasksTitle);
+
+const currentTasks = document.createElement("div");
+currentTasks.className = "to-do-boxes";
+currentTaskContainer.appendChild(currentTasks);
+
+const currentTaskList = document.createElement("ul");
+currentTaskList.id = "current-tasks-ul";
+
+const addTaskLi = document.createElement("li");
+addTaskLi.className = "add-task-li";
+addTaskLi.textContent = "";
+
+// const addTaskContainer = document.createElement("div");
+// addTaskContainer.className = "taskbar";
+
+// implement soon
+// const addTaskInput = document.createElement("input");
+// addTaskInput.placeholder = "Add a task to complete"
+
+const addTaskBtn = document.createElement("div");
+addTaskBtn.textContent = "+";
+addTaskBtn.id = "add-task-btn";
+currentTaskList.appendChild(addTaskBtn);
+
+// task details DOM
+
+const taskDetailsContainer = document.createElement("div");
+taskDetailsContainer.id = "task-details";
+
+const taskDetailsTitle = document.createElement("h2");
+taskDetailsTitle.className = "to-do-title";
+taskDetailsTitle.textContent = "Details";
+taskDetailsContainer.appendChild(taskDetailsTitle);
+
+const taskDetails = document.createElement("div");
+taskDetails.className = "to-do-boxes";
+taskDetailsContainer.appendChild(taskDetails);
+
 // add project to DOM
 addProjectButton.addEventListener("click", DOMProjectAdder);
 
@@ -49,11 +103,19 @@ for (let i = 0; i < window.projects.length; i++) {
   projectListSetter(projects[i], projectsList);
 }
 
+// appends
 projectsList.appendChild(addProjectButton);
 projectsContainer.appendChild(projectsList);
 body.appendChild(topBar);
 topBar.appendChild(appTitle);
 body.appendChild(leftSideBar);
+
+currentContainer.appendChild(currentTaskContainer);
+currentContainer.appendChild(taskDetailsContainer);
+
+todoBoardsContainer.appendChild(currentContainer);
+body.appendChild(todoBoardsContainer);
+
 leftSideBar.appendChild(projectsTitle);
 leftSideBar.appendChild(projectsContainer);
 
