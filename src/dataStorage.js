9334cae0d12ff3aa8f662f92098a24a4daff49a6
xandre9-git <jmjames673@gmail.com>
2023-02-04 11:14:40 -0500

    Create object constructor for use in task creations

--- src/dataStorage.js
+++ src/dataStorage.js
@@ -1,8 +1,33 @@
+// project names array
 let projects = JSON.parse(window.localStorage.getItem("projectnames"));
-// let projects = [];
 
 // create global reference to array
 window.projects = projects;
+
+// task object creator
+
+function Task(project, desc, dateDue, priority) {
+  this.project = project
+  this.desc = desc;
+  this.dateDue = dateDue;
+  this.priority = priority;
+  this.commands = function commands(){
+    // placeholder
+  }
+}
+
+// example task
+
+const practiceProgramming = new Task(
+  "Odin",
+  "Practice programming for two hours",
+  new Date(2023, 1, 3)
+)
+console.log(`practiceProgramming: ${practiceProgramming.project} ${practiceProgramming.desc} ${practiceProgramming.dateDue}`);
+
+// to-do array
+// will store objects as items
+let toDos = [];
 
 export { projects };
 
